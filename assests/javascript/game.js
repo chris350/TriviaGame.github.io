c65bd2b3f7e68577936b40b0750a21982021ea82
pradnya <pradnyahegde@gmail.com>
2019-09-20 22:08:20 -0400

    added function displayQuestions() to display question and answer options

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+  var questions = [
+    {
+    statement:"First question goes here",
+    answers:["answer1.1","answer1.2","answer1.3","answer1.4"],
+    correct:"answer1.2",
+    divClass:"OneOOne"
+    },
+    {
+    statement:"Second question goes here",
+    answers:["answer2.1","answer2.2","answer2.3","answer2.4"],
+    correct:"answer2.4",
+    divClass:"TwoOOne" 
+    },
+    {
+      statement:"Third question goes here",
+      answers:["answer3.1","answer3.2","answer3.3","answer3.4"],
+      correct:"answer3.3",
+      divClass:"ThreeOOne"   
+    }
+  ];
+  var totalQuestions=3;
+  var labels = ["first", "second", "third", "forth"];
+
+  //Display trivia questions on page
+  function displayQuestion(){
+    for(var i= 0; i < totalQuestions ; i++)
+    {
+      $(".form-group").prepend("<div class="+questions[i].divClass+">"+questions[i].statement+"</div>");
+      let answersArrayLen=questions[i].answers.length;     
+       for(var x= 0;x < answersArrayLen; x++){
+        $("."+questions[i].divClass).append("<div class='form-check'><input class='form-check-input' type='radio' name="+questions[i].divClass+" value="+questions[i].answers[x]+"><label class='form-check-label' for="+labels[x]+">"+questions[i].answers[x]+"</label></div>");
+      }
+    }//for i<totalQuestions
+  }
 
     //countdown 
     var timeleft = 30;
@@ -9,16 +43,12 @@ $(document).ready(function() {
         if(timeleft <= 0){
           clearInterval(downloadTimer);
           console.log("After clear interval...")
-          //document.getElementById("countdown").innerHTML = "Finished"
         }}, 1000);
     }
 
-    //On button click event
-$("#submitButton").on("click", function() {
-});
 
 
 initiateCountdown();
-
+displayQuestion();
 
 }); //document.ready
