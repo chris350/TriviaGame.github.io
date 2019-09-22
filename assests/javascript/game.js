cc8f639f8cd68f8035a7a2ae65ead5413a195dfe
pradnya <pradnyahegde@gmail.com>
2019-09-21 22:39:10 -0400

    added quiz questions and changed class form-check to radio-inline

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,35 +1,68 @@
 $(document).ready(function() {
   var questions = [
     {
-    statement:"First question goes here",
-    answers:["answer1.1","answer1.2","answer1.3","answer1.4"],
-    correct:"answer1.2",
-    divClass:"OneOOne"
+    statement:"Which is the largest country in the world by land mass?",
+    answers:["China","Russia","United States","Brazil"],
+    correct:"Russia",
+    divClass:"countries"
     },
     {
-    statement:"Second question goes here",
-    answers:["answer2.1","answer2.2","answer2.3","answer2.4"],
-    correct:"answer2.4",
-    divClass:"TwoOOne" 
+    statement:"Which continent contains the largest number of countries?",
+    answers:["Africa","Asia","South America","Europe"],
+    correct:"Africa",
+    divClass:"continent" 
     },
     {
-      statement:"Third question goes here",
-      answers:["answer3.1","answer3.2","answer3.3","answer3.4"],
-      correct:"answer3.3",
-      divClass:"ThreeOOne"   
+      statement:"Which country has highest young population?",
+      answers:["Japan","India","Australia","Malawi"],
+      correct:"Malawi",
+      divClass:"youngcountry"   
+    },
+    {
+      statement:"In what continent is the ancient city of Timbuktu?",
+      answers:["Africa","Asia","Europe","Australia"],
+      correct:"Africa",
+      divClass:"tibuktu"   
+    },
+    {
+      statement:"Which is the worlds largest freshwater lake?",
+      answers:["Lake Victoria","Lake Superior","Lake Huron","Lake Baikal"],
+      correct:"Lake Superior",
+      divClass:"lake"   
+    },
+    {
+      statement:"Which year NASA land Spirit and Opportunity rovers on Mars?",
+      answers:["2012","1996","2006","2004"],
+      correct:"2004",
+      divClass:"space"   
+    },
+    {
+      statement:"What is baby shark called?",
+      answers:["calf","joey","pup","sharklet"],
+      correct:"pup",
+      divClass:"babyshark"   
+    },
+    {
+      statement:"Whis the tallest mountain on the Earth?",
+      answers:["Mount Everest","Mount Washington","Mount Whitney","Mount St. Helens"],
+      correct:"Mount Everest",
+      divClass:"mountain"   
     }
   ];
-  var totalQuestions=3;
-  var labels = ["first", "second", "third", "forth"];
+  var totalQuestions=8;
+  var labels = ["first", "second", "third", "forth","fifth","sixth","seventh","eighth"];
 
   //Display trivia questions on page
   function displayQuestion(){
     for(var i= 0; i < totalQuestions ; i++)
     {
-      $(".form-group").prepend("<div class="+questions[i].divClass+">"+questions[i].statement+"</div>");
+      $(".form-group").prepend("<div class="+questions[i].divClass+"></div>");
+      var divQuestion=$('<div class=statement>'+questions[i].statement+'</div>');
+      $('.'+questions[i].divClass).append(divQuestion);
+
       let answersArrayLen=questions[i].answers.length;     
        for(var x= 0;x < answersArrayLen; x++){
-        $("."+questions[i].divClass).append("<div class='form-check'><input class='form-check-input' type='radio' name="+questions[i].divClass+" value="+questions[i].answers[x]+"><label class='form-check-label' for="+labels[x]+">"+questions[i].answers[x]+"</label></div>");
+        $("."+questions[i].divClass).append("<div class='radio-inline'><input class='form-check-input' type='radio' name="+questions[i].divClass+" value="+questions[i].answers[x]+"><label class='form-check-label' for="+labels[x]+">"+questions[i].answers[x]+"</label></div>");
       }
     }//for i<totalQuestions
   }
